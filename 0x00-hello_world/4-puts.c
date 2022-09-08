a4d86bfe3678409c58af975c6d715a35d827ff1e
Veektor22 <vickza8@gmail.com>
2022-09-08 14:54:17 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *
+ * * main - entry point
+ *
+ * * Return: 0 after printing the function
+ *
+ * */
+
+int main(void)
+
+{puts("\"programmimg is like building a multilingual puzzle");
+
+		return (0);
+
+}
