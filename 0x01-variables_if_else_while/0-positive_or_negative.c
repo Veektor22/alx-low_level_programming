bb08715a30405708e9d2c19426c46b6cdf2a2317
Veektor22 <vickza8@gmail.com>
2022-09-15 13:08:13 -0700

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,9 @@
 /**
 * main - print if the number is positive, zero, or negative
 * Description: using the main function
-* this program prints "Programming is positive, zero, or negative
-* Return: 0 */
-
+* this program prints "Programming is positive, zero, or negative"
+* Return: 0 
+*/
 int main(void)
 {
 int n;
