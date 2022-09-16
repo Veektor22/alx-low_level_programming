4dc9622b45ab3444a314fac8174a4a530605f971
Veektor22 <vickza8@gmail.com>
2022-09-15 23:20:58 -0700

    Task 8 - Task 9

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-#include <studio.h>
+#include <stdio.h>
 
 /**
 * main - print if the number is positive, zero, or negative
@@ -11,10 +11,10 @@
 int main(void)
 {
 char c;
-for (c = 'z'; c >= 'a'; c--)
+for(c = 'z'; c >= 'a'; c--)
 {
 putchar(c);
 }
-putchar ("\n");
-return (0);
+putchar("\n");
+return(0);
 }
