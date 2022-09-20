160cce444eac2e5efd20e1fd4daa905755b09907
Veektor22 <vickza8@gmail.com>
2022-09-20 05:10:25 -0700

    I can only go one way. Ive not got a reverse gear

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,7 +10,7 @@
 int _strlen(char *s)
 {
 int u = 0;
- while (*(s + len) != '\0')
+ while (*(s + u) != '\0')
 u++;
 
 return (u);
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_print_rev - print a string in reverse order
+ *@s: A pointer to an int that will be changed
+ *
+ *Return: void which means our answer is correct
+ */
+
+void print_rev(char *s)
+int i;
+i = 0;
+while (s[i] != '0')
+{
+i++;
+}
