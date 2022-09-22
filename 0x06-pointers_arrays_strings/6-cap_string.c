9b41a67cc71d14e7a55df142efca07853e6125bb
Veektor22 <vickza8@gmail.com>
2022-09-22 04:17:21 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,6 @@
 
 char *cap_string(char *x)
 {
-x = "Victor, is learn programming africa"
   char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
   int len = 13;
   int a = 0, i;
@@ -16,8 +15,8 @@ while (x[a])
 i = 0;
 while (i < len)
 {
-if ((a == 0 || s[a-1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-s[a] = s[a] - 32;
+if ((a == 0 || x[a-1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
+x[a] = x[a] - 32;
 i++;
 }
 a++;
